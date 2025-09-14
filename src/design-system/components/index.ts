ad55611baa992290b71a1cd4a446e7231bec65ea
skumar <kumasachin@gmail.com>
2025-09-14 16:47:46 +0100

    config fix

--- src/design-system/components/index.ts
+++ src/design-system/components/index.ts
@@ -1,1 +1,15 @@
-export { Button } from "./Button";
+// Original styled-components Button
+export { Button as StyledButton } from "./Button";
+
+// MUI Wrapper Components
+export { Button } from "./MuiButton";
+export type { ButtonProps } from "./MuiButton";
+
+export { TextField } from "./TextField";
+export type { TextFieldProps } from "./TextField";
+
+export { Card } from "./Card";
+export type { CardProps } from "./Card";
+
+export { Typography } from "./Typography";
+export type { TypographyProps } from "./Typography";
