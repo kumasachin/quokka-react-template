74b9570ce1469e457351eb31b911811981e00408
skumar <kumasachin@gmail.com>
2025-09-27 01:21:43 +0100

    fix

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 import { AuthUser, useAuth } from "../hooks/useAuth";
 
 interface AuthContextType {
